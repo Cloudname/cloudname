8e5b3199ee97c7199504ce71160319b7e7171984
Bjorn Borud <bborud@gmail.com>
2011-11-28 17:12:56 +0100

    Reimplemented escape() for 2x speedup
    * Replaced the escape() method with a much simpler method that
      produces on the order of 2x speedup.
    
    * Moved the utility functions to Util and made them public

--- log/src/main/java/org/cloudname/log/format/SingleLineFormatter.java
+++ log/src/main/java/org/cloudname/log/format/SingleLineFormatter.java
@@ -2,12 +2,6 @@ package org.cloudname.log.format;
 
 import org.cloudname.log.pb.Timber;
 
-import java.util.TreeMap;
-
-import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Format Timber.LogEvent messages on a single line using NL to
  * separate log records and TAB to separate fields. The fields are, in
@@ -28,33 +22,12 @@ import java.util.regex.Pattern;
  * @author borud
  */
 public class SingleLineFormatter implements LogEventFormatter {
-    // Patterns used by the escape() method.
-    private static Pattern special   = Pattern.compile("[\\\n\\\t\\\\]+");
-    private static Pattern newLine   = Pattern.compile("\n");
-    private static Pattern tab       = Pattern.compile("\t");
-    private static Pattern backSlash = Pattern.compile("\\\\");
-
-    private static final TreeMap<Integer,String> logLevelByValue = new TreeMap<Integer,String>();
-
-    static {
-        logLevelByValue.put(Level.CONFIG.intValue(), Level.CONFIG.getName());
-        logLevelByValue.put(Level.FINE.intValue(), Level.FINE.getName());
-        logLevelByValue.put(Level.FINER.intValue(), Level.FINER.getName());
-        logLevelByValue.put(Level.FINEST.intValue(), Level.FINEST.getName());
-        logLevelByValue.put(Level.INFO.intValue(), Level.INFO.getName());
-        logLevelByValue.put(Level.SEVERE.intValue(), Level.SEVERE.getName());
-        logLevelByValue.put(Level.WARNING.intValue(), Level.WARNING.getName());
-        logLevelByValue.put(Integer.MAX_VALUE, "NUCLEAR");
-    }
-
-
-
     @Override
     public String format(Timber.LogEvent logEvent) {
         StringBuilder buff = new StringBuilder(200);
 
         // Format the timestamp and append it to the buffer
-        formatTimeSecondsSinceEpoch(logEvent.getTimestamp(), buff);
+        Util.formatTimeSecondsSinceEpoch(logEvent.getTimestamp(), buff);
 
         buff.append('\t')
             .append(logEvent.getHost())
@@ -79,7 +52,7 @@ public class SingleLineFormatter implements LogEventFormatter {
             .append('\t')
 
             // Level of log message
-            .append(logLevelNameForValue(logEvent.getLevel()))
+            .append(Util.logLevelNameForValue(logEvent.getLevel()))
             .append('\t')
 
             // Add consistency level
@@ -89,104 +62,15 @@ public class SingleLineFormatter implements LogEventFormatter {
         // Add the payloads
         boolean first = true;
         for (Timber.Payload payload : logEvent.getPayloadList()) {
-            if (! first) {
-                buff.append(" | ");
-            }
-            first = false;
-
-            buff.append(payload.getName())
+            buff.append((first?"":", "))
+                .append(payload.getName())
                 .append(": ")
-                .append(escape(payload.getPayload().toStringUtf8()));
+                .append(Util.escape(payload.getPayload().toStringUtf8()));
+
+            first = false;
         }
+
 
         return buff.toString();
     }
-
-
-    /**
-     * Convert timestamp to seconds since epoch with 3 decimal places.
-     *
-     * @param time the time as milliseconds since epoch
-     * @param sbuffer a StringBuilder used to put the formatted number into
-     */
-    private static void formatTimeSecondsSinceEpoch (long time, StringBuilder sbuffer) {
-        String timeString = Long.toString(time);
-        int len = timeString.length();
-
-        // something wrong.  handle it by just returning the input
-        // long as a string.  we prefer this to just crashing in
-        // the substring handling.
-        if (len < 3) {
-            sbuffer.append(timeString);
-            return;
-        }
-
-        sbuffer.append(timeString.substring(0, len - 3));
-        sbuffer.append('.');
-        sbuffer.append(timeString.substring(len - 3));
-    }
-
-    /**
-     * This static method is used to detect if a message needs
-     * to be escaped, and if so, performs the escaping.  Since the
-     * common case is most likely that escaping is <em>not</em>
-     * needed, the code is optimized for this case.  The forbidden
-     * characters are:
-     *
-     * <UL>
-     *  <LI> newline
-     *  <LI> tab
-     *  <LI> backslash
-     * </UL>
-     *
-     * <P>
-     * Also handles the case where the message is <code>null</code>
-     * and replaces the null message with a tag saying that the
-     * value was "(empty)".
-     *
-     * @param s String that might need escaping
-     * @return Returns escaped string
-     *
-     */
-    public static String escape (String s) {
-        if (s == null) {
-            return "(empty)";
-        }
-
-        Matcher m = special.matcher(s);
-        if (! m.find()) {
-            return s;
-        }
-
-        // invariant: we had special characters
-
-        m = backSlash.matcher(s);
-        if (m.find()) {
-            s = m.replaceAll("\\\\\\\\");
-        }
-
-        m = newLine.matcher(s);
-        if (m.find()) {
-            s = m.replaceAll("\\\\n");
-        }
-
-        m = tab.matcher(s);
-        if (m.find()) {
-            s = m.replaceAll("\\\\t");
-        }
-
-        return s;
-    }
-
-
-
-
-
-    /**
-     *
-     */
-    private static String logLevelNameForValue(int value) {
-        return logLevelByValue.ceilingEntry(value).getValue();
-    }
-
 }
---
+++ log/src/main/java/org/cloudname/log/format/Util.java
@@ -0,0 +1,106 @@
+package org.cloudname.log.format;
+
+import java.util.TreeMap;
+import java.util.logging.Level;
+
+
+/**
+ * Utilities used by formatting implementations.
+ *
+ * @author borud
+ */
+public class Util {
+
+    // There is no convenient way to map numeric values of log levels
+    // to names, so we use the log levels defined in java.util.logging
+    // and we add them to the map.  Since we use the TreeMap to look
+    // up the ceiling of each value we have added a bogus log level
+    // called "NUCLEAR".
+    private static final TreeMap<Integer,String> logLevelByValue = new TreeMap<Integer,String>();
+    static {
+        logLevelByValue.put(Level.CONFIG.intValue(), Level.CONFIG.getName());
+        logLevelByValue.put(Level.FINE.intValue(), Level.FINE.getName());
+        logLevelByValue.put(Level.FINER.intValue(), Level.FINER.getName());
+        logLevelByValue.put(Level.FINEST.intValue(), Level.FINEST.getName());
+        logLevelByValue.put(Level.INFO.intValue(), Level.INFO.getName());
+        logLevelByValue.put(Level.SEVERE.intValue(), Level.SEVERE.getName());
+        logLevelByValue.put(Level.WARNING.intValue(), Level.WARNING.getName());
+        logLevelByValue.put(Integer.MAX_VALUE, "NUCLEAR");
+    }
+
+    /**
+     * This is a non-instantiable class.
+     */
+    private Util() {}
+
+    /**
+     * Will find the first named log level higher than or equal to the
+     * numberic value.
+     *
+     * @param value the numeric value of a log level
+     * @return the name of the log level that is equal to or higher
+     *   than the value we provided.
+     */
+    public static String logLevelNameForValue(int value) {
+        return logLevelByValue.ceilingEntry(value).getValue();
+    }
+
+    /**
+     * Convert timestamp to seconds since epoch with 3 decimal places.
+     *
+     * @param time the time as milliseconds since epoch
+     * @param sbuffer a StringBuilder used to put the formatted number into
+     */
+    public static void formatTimeSecondsSinceEpoch (long time, StringBuilder sbuffer) {
+        String timeString = Long.toString(time);
+        int len = timeString.length();
+
+        // something wrong.  handle it by just returning the input
+        // long as a string.  we prefer this to just crashing in
+        // the substring handling.
+        if (len < 3) {
+            sbuffer.append(timeString);
+            return;
+        }
+
+        sbuffer.append(timeString.substring(0, len - 3));
+        sbuffer.append('.');
+        sbuffer.append(timeString.substring(len - 3));
+    }
+
+    /**
+     * Replace NL, TAB and Backslash with their escaped versions.  (We
+     * could probably speed up this method a bit more by replacing the
+     * disallowed characters with SPACE rather than escaping them.)
+     *
+     * @param s the String we wish to escape
+     * @return a String guaranteed to not contain NL, TAB or single backslashes
+     */
+    public static String escape(String s) {
+        // Guesstimate on length.  Doesn't need to be accurate, only
+        // probable
+        StringBuilder buff = new StringBuilder(s.length() + 20);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch(c) {
+                case '\n':
+                    buff.append("\\n");
+                    break;
+
+                case '\t':
+                    buff.append("\\t");
+                    break;
+
+                case '\\':
+                    buff.append("\\\\");
+                    break;
+
+                default:
+                    buff.append(c);
+            }
+        }
+
+        return buff.toString();
+    }
+}
