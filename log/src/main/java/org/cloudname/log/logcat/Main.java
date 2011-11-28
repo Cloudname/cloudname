afaceb9f0d288001e185264646293743c9862919
Bjorn Borud <bborud@gmail.com>
2011-11-28 22:20:37 +0100

    * temporary Main

--- log/src/main/java/org/cloudname/log/logcat/Main.java
+++ log/src/main/java/org/cloudname/log/logcat/Main.java
@@ -3,6 +3,9 @@ package org.cloudname.log.logcat;
 import static org.cloudname.log.pb.Timber.LogEvent;
 import static org.cloudname.log.pb.Timber.Payload;
 
+import org.cloudname.log.format.LogEventFormatter;
+import org.cloudname.log.format.SingleLineFormatter;
+
 import org.cloudname.log.recordstore.RecordReader;
 
 import java.io.FileInputStream;
@@ -15,15 +18,32 @@ import java.io.FileInputStream;
 public class Main {
     public static void main(String[] args) throws Exception {
         for (String filename : args) {
-            prettyPrint(filename);
+            prettyPrint(filename, new SingleLineFormatter(), true);
         }
     }
 
-    private static void prettyPrint(String filename) throws Exception {
+    private static void prettyPrint(String filename, LogEventFormatter formatter, boolean tail)
+        throws Exception
+    {
         RecordReader reader = new RecordReader(new FileInputStream(filename));
-        LogEvent logEvent = null;
-        while ((logEvent = reader.read()) != null) {
-            // output
+        while (true) {
+            LogEvent logEvent = reader.read();
+
+            if (logEvent != null) {
+                System.out.println(formatter.format(logEvent));
+                continue;
+            }
+
+            // Invariant: read() returned null.  If we are in tail mode
+            // we simply sleep a bit and then try again.
+            if (tail) {
+                Thread.sleep(200);
+                continue;
+            }
+
+            // Invariant: read() returned null and we are not in tail mode
+            reader.close();
+            return;
         }
     }
 }
