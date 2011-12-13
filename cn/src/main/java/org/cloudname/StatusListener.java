ce139d981311c4b4e38c1b2e1e083345237e53e1
Haakon Dybdahl <dybdahl@comoyo.com>
2011-12-13 08:03:18 +0100

    First attempt.

---
+++ cn/src/main/java/org/cloudname/StatusListener.java
@@ -0,0 +1,11 @@
+package org.cloudname;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dybdahl
+ * Date: 12/12/11
+ * Time: 12:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StatusListener {
+}
---
+++ cn/src/main/java/org/cloudname/zk/ZooKeeperWrapper.java
@@ -0,0 +1,11 @@
+package org.cloudname.zk;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dybdahl
+ * Date: 12/12/11
+ * Time: 8:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ZooKeeperWrapper {
+}
